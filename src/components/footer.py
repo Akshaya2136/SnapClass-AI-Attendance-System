d4d58f9052448b001dc86ee0da5fe9d7504775ff
Akshaya Yammadi <akshayayammadi06@gmail.com>
2026-07-28 16:18:25 +0530

    Update app domain for deployment

--- src/components/footer.py
+++ src/components/footer.py
@@ -7,7 +7,7 @@ def footer_home():
 
     st.markdown(f"""
         <div style = "margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by Akshaya Yammadi</p>
+        <p style="font-weight:bold; color:white;"> Created by Akshaya Yammadi❤️</p>
         </div>
                 """,unsafe_allow_html = True)
     
@@ -19,6 +19,6 @@ def footer_dashboard():
 
     st.markdown(f"""
         <div style = "margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by Akshaya Yammadi</p>
+        <p style="font-weight:bold; color:black;"> Created by Akshaya Yammadi❤️</p>
         </div>
                 """,unsafe_allow_html = True)
